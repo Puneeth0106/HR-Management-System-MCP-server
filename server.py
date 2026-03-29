725c8ba398c4f7e78f5813dab9ffd88873f7208c
Puneeth0106 <puneethkumaramudala7@gmail.com>
2026-03-29 19:13:41 -0400

    Updated server.py

--- server.py
+++ server.py
@@ -2,4 +2,27 @@ from fastmcp import FastMCP
 
 mcp= FastMCP("Atliq-HR-assist")
 
+#tools
 @mcp.tool()
+def add_employee(name: s)
+
+
+
+
+
+
+
+
+
+
+
+
+#resources
+
+#prompts
+
+
+
+
+if __name__ == "__main__":
+    mcp.run(transport="stdio")
